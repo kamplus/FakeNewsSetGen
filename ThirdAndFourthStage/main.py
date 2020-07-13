687ee04acb96230e054e417d390b800c5dd5e066
kamplus <flaviormsilva@gmail.com>
2020-07-13 17:21:35 -0300

    Remove bug path SocialMediaCollector

--- ThirdAndFourthStage/main.py
+++ ThirdAndFourthStage/main.py
@@ -35,7 +35,7 @@ class DataCollectorFactory:
 
 
 def init_config():
-    json_object = json.load(open("./SocialMediaCollector/config.json"))
+    json_object = json.load(open("./config.json"))
 
     config = Config(json_object["dataset_dir"], json_object["dump_location"], json_object["tweet_keys_file"],
                     int(json_object["num_process"]))
